9e62dfce504cf2a3673b9d1419f6dd952fda3fb2
Steven Miller <steven.miller.msm@gmail.com>
2021-09-26 10:58:50 -0400

    Add a client and server test

---
+++ src/test/java/com/wiligsi/plump/ServerAndClientTests.java
@@ -0,0 +1,69 @@
+package com.wiligsi.plump;
+
+
+import com.wiligsi.plump.server.PlumpImpl;
+import io.grpc.ManagedChannel;
+import io.grpc.Server;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.inprocess.InProcessServerBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import static com.wiligsi.plump.PlumpOuterClass.*;
+import static org.assertj.core.api.Assertions.*;
+
+public class ServerAndClientTests {
+    private Server plumpServer;
+    private ManagedChannel plumpChannel;
+    private PlumpGrpc.PlumpBlockingStub plumpBlockingStub;
+
+    @BeforeEach
+    public void createInProcessServerClient() throws IOException {
+        String serverName = InProcessServerBuilder.generateName();
+        plumpServer = InProcessServerBuilder.forName(serverName)
+                .directExecutor()
+                .addService(new PlumpImpl())
+                .build();
+        plumpServer.start();
+
+        plumpChannel = InProcessChannelBuilder.forName(serverName)
+                .directExecutor()
+                .build();
+
+        plumpBlockingStub = PlumpGrpc.newBlockingStub(
+                plumpChannel
+        );
+
+    }
+
+    @AfterEach
+    public void terminateInProcessServerClient() throws InterruptedException {
+        plumpChannel.shutdown();
+        plumpServer.shutdown();
+
+        try {
+            assert plumpChannel.awaitTermination(5, TimeUnit.SECONDS) : "Channel could not be shutdown in reasonable time";
+            assert plumpServer.awaitTermination(5, TimeUnit.SECONDS) : "Server could not be shutdown in reasonable time";
+        } finally {
+            plumpChannel.shutdownNow();
+            plumpServer.shutdownNow();
+        }
+    }
+
+    @Test
+    public void itShouldBePossibleToCreateALock() {
+        assertThatCode(
+                () -> {
+                    CreateLockReply reply = plumpBlockingStub.createLock(
+                            CreateLockRequest.newBuilder()
+                                    .setLockName("TestLock")
+                                    .build()
+                    );
+                }
+        ).doesNotThrowAnyException();
+    }
+}
